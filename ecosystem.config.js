76e609b111c6339501d89f28dde0d533ee146cc4
denis_polyarush <55382692+DenisDolmatov2020@users.noreply.github.com>
2021-10-24 14:30:05 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'nuxt',
+      name: 'lottee_frontend',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
