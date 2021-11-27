9d670927e6e087970f5071fe2951c3d301cf58c7
denis_polyarush <55382692+DenisDolmatov2020@users.noreply.github.com>
2021-11-27 14:42:05 +0700

    Remove port from ws Uri

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,7 +88,7 @@ export default {
       console.log('PORT LOC', window.location.port)
       const protocol = (window.location.protocol === 'https:') ? 'wss:' : 'ws:'
 
-      const wsUri = protocol + '://' + window.location.host + ':8001/'
+      const wsUri = protocol + '://' + window.location.host
       const prizeSocket = new WebSocket(wsUri || process.env.WS_URI + '/ws/prize/')
 
       prizeSocket.onmessage = ({ data }) => {
